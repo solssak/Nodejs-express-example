dbcc4782c354970c8c0bb37707c12a7eefbdaff2
solssak <gswoong1012@gmail.com>
2023-04-05 21:54:12 +0900

    feat: cors 실습

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.get("/sound/:name", (req, res) => {
   const { name } = req.params;
